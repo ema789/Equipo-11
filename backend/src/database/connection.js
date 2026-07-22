0d7af602f7a79973ad7001ef5bf89fe36f1011db
ema789 <emasanchez473@outlook.com>
2026-07-22 16:28:40 -0300

    FIX: Agrego SSL a la conexion de PostgreSQL con pg 1

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -5,15 +5,17 @@ dotenv.config();
 
 const { Pool } = pg; 
 
+// Verificamos si estamos en Render (o producción) o en local
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  // Solo aplicamos SSL si estamos en producción (Render)
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 export default pool;
