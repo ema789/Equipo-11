7395a31bd0820d4a2b3403bca9a4cd354abfe8d9
ema789 <emasanchez473@outlook.com>
2026-07-22 16:00:47 -0300

    FIX: Generando el deply en render 1

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./src/app.js";
-import { connectDB } from "./src/config/dataBase.config.js";
+import { connectDB } from "./src/config/database.config.js";
 
 const PORT = process.env.PORT || 4000;
 
